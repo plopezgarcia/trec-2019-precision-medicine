e962307dc1afeb247f3cdb28a37951f805045f6a
Pablo Lopez <plopezgarcia@gmail.com>
2019-06-09 17:07:19 +0200

    updated to 2019

--- src/main/java/at/medunigraz/imi/bst/config/TrecConfig.java
+++ src/main/java/at/medunigraz/imi/bst/config/TrecConfig.java
@@ -1,7 +1,5 @@
 package at.medunigraz.imi.bst.config;
 
-import java.util.Arrays;
-import java.util.List;
 import java.util.ResourceBundle;
 
 public final class TrecConfig {	
@@ -22,31 +20,15 @@ public final class TrecConfig {
 
 
     /* DATA - MEDLINE */
-    public static final String MEDLINE_FOLDER_COMPRESSED = "/Volumes/PabloSSD/trec/medline_xml_all/compressed/";
-    public static final String MEDLINE_FOLDER_PLAIN = "/Volumes/PabloSSD/trec/medline_xml_all/";
 
-    public static final String MEDLINE_FILE_PATTERN = "medline17n0%03d.xml";
-    public static final int MEDLINE_FILE_START = 1;
-    public static final int MEDLINE_FILE_END = 889;
+    public static final String MEDLINE_FILE_PATTERN = "pubmed19n0%03d.xml";
 
-    //String DATA_FOLDER = "/Users/plopez/Desktop/trec/";
-    public static final String DATA_FOLDER = "/Volumes/PabloSSD/trec/medline_xml_all/";
+    public static final int MEDLINE_FILE_END = 972;
+
 
     public static final String SAMPLE_SMALL_XML = "src/main/resources/data/medline-sample.xml";
     public static final String SAMPLE_EXTRA_ABSTRACT_TXT = "src/main/resources/data/extra-abstract-sample.txt";
 
-    public static final List<String> SAMPLE_MULTI_XML = Arrays.asList(
-                                                                    DATA_FOLDER + "uncompressed/medline17n0050.xml",
-                                                                    DATA_FOLDER + "uncompressed/medline17n0100.xml",
-                                                                    DATA_FOLDER + "uncompressed/medline17n0189.xml",
-                                                                    DATA_FOLDER + "uncompressed/medline17n0201.xml",
-                                                                    DATA_FOLDER + "uncompressed/medline17n0355.xml",
-                                                                    DATA_FOLDER + "uncompressed/medline17n0492.xml",
-                                                                    DATA_FOLDER + "uncompressed/medline17n0519.xml",
-                                                                    DATA_FOLDER + "uncompressed/medline17n0666.xml",
-                                                                    DATA_FOLDER + "uncompressed/medline17n0739.xml",
-                                                                    DATA_FOLDER + "uncompressed/medline17n0889.xml"
-                                                        );
 	
 	public static String getString(String key) {
 		return PROPERTIES.getString(key);
