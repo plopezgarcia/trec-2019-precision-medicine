dfeb58f4dd25320a68707c2121cbb5cbdb3fcd14
jverara <joseavr1992@gmail.com>
2019-07-12 10:55:20 +0200

    Added new column for extra inf and separate fusion genes and variations

--- python-experiments/trec_utils/utils.py
+++ python-experiments/trec_utils/utils.py
@@ -6,11 +6,14 @@ import pandas
 import pytrec_eval
 from sklearn.model_selection import train_test_split
 
+
 def load_config():
     with open(os.path.dirname(__file__) + '/config.json', 'r') as f:
         return(json.load(f))
 
 # Converts numerical age to MeSH age groups (adapted)
+
+
 def to_age_group(years_of_age):
     age = int(years_of_age)
     if age < 2:
@@ -24,8 +27,11 @@ def to_age_group(years_of_age):
     return('aged')
 
 # FIXME: Remove weird things like "amplification, etc..." which give a worse score
+
+
 def get_topics(topics_file):
-    input_topics = xml.etree.ElementTree.parse(topics_file).getroot().findall('topic')
+    input_topics = xml.etree.ElementTree.parse(
+        topics_file).getroot().findall('topic')
     topics_dict = {}
     for t in input_topics:
         topic = int(float(t.get('number')))
@@ -33,32 +39,56 @@ def get_topics(topics_file):
         gene = t.find('gene').text
         demographic = t.find('demographic').text
         gene_list = gene.split(',')
-        gene1 = gene_list[0]
-        gene2 = ''
-        gene3 = ''
-        if (len(gene_list)>1):
-            gene2 = gene_list[1]
-        if (len(gene_list)>2):
-            gene3 = gene_list[2]
+
+        if 'fusion' in gene_list[0]:
+            genes, fusion = gene_list[0].split(' ')
+            genes = genes.split('-')
+            gene1 = genes[0]
+            gene2 = genes[1]
+            extra_inf = fusion #add columns for extra information in case there are commas after fusion
+            if (len(gene_list) > 1):
+            	gene3 = gene_list[1]
+            else:
+            	gene3 = ''
+        elif re.search(r'\((.*?)\)', gene_list[0]):
+            genes = gene_list[0].split('(') # fix split. maybe spaces within parenthesis
+            gene1 = genes[0]
+            gene2 = re.findall('(.*?)\)', genes[1])[0]
+            extra_inf = 'variation' #add columns for extra information in case there are commas after variation
+            if (len(gene_list) > 1):
+            	gene3 = gene_list[1]
+            else:
+            	gene3 = ''
+        else:
+            gene1 = gene_list[0]
+            gene2 = ''
+            gene3 = ''
+            extra_inf = ''
+            if (len(gene_list) > 1):
+                gene2 = gene_list[1]
+            if (len(gene_list) > 2):
+                gene3 = gene_list[2]
+
         if 'female' in demographic:
             sex = 'female'
         else:
             sex = 'male'
-        age = re.findall('\\d+',demographic)[0]
+        age = re.findall('\\d+', demographic)[0]
 
-        topics_dict[topic] = {  'topic': topic,
-                                'disease': disease,
-                                'gene': gene,
-                                'gene1': gene1,
-                                'gene2': gene2,
-                                'gene3': gene3,
-                                'sex': sex,
-                                'age': age,
-                                'age_group': to_age_group(age)}
+        topics_dict[topic] = {'topic': topic,
+                              'disease': disease,
+                              'gene': gene,
+                              'gene1': gene1,
+                              'gene2': gene2,
+                              'gene3': gene3,
+                              'extra_inf': extra_inf,
+                              'sex': sex,
+                              'age': age,
+                              'age_group': to_age_group(age)}
 
     topics_df = pandas.DataFrame.from_dict(topics_dict, orient='index')
-    topics_df  = topics_df [['topic', 'disease', 'gene',
-                             'gene1', 'gene2', 'gene3', 'sex', 'age', 'age_group']]
+    topics_df = topics_df[['topic', 'disease', 'gene',
+                           'gene1', 'gene2', 'gene3', 'extra_inf', 'sex', 'age', 'age_group']]
     return(topics_df)
 
 def get_qrels_as_dict(qrel_file):
@@ -67,6 +97,7 @@ def get_qrels_as_dict(qrel_file):
         qrels = pytrec_eval.parse_qrel(f_qrel)
     return(qrels)
 
+
 def get_qrels_as_df(qrels_file):
     qrels = get_qrels_as_dict(qrels_file)
     qrels_tuples_list = []
@@ -74,11 +105,14 @@ def get_qrels_as_df(qrels_file):
         for doc, relevance in doc_relevance.items():
             qrels_tuples_list.append((int(topic), doc, int(relevance)))
 
-    qrels_df = pandas.DataFrame(columns=['topic','doc_id','relev'], data=qrels_tuples_list)
-    return(qrels_df.sort_values(['topic','relev'], ascending=[True, False]))
+    qrels_df = pandas.DataFrame(
+        columns=['topic', 'doc_id', 'relev'], data=qrels_tuples_list)
+    return(qrels_df.sort_values(['topic', 'relev'], ascending=[True, False]))
+
 
 def get_qrels(qrels_file):
     return(get_qrels_as_df(qrels_file))
+
 
 def run_to_pytrec_eval(df):
     run_dict_pytrec_eval = {}
@@ -92,31 +126,36 @@ def run_to_pytrec_eval(df):
 # eval.get_qrels_as_dict('./gold-standard/abstracts.2017.qrels') ==
 # eval.qrels_to_pytrec_eval(eval.get_qrels_as_df(('./gold-standard/abstracts.2017.qrels')))
 
+
 def qrels_to_pytrec_eval(qrels_df):
     qrels_dict = {}
     for index, qrel_row in qrels_df.iterrows():
         if str(qrel_row['topic']) not in qrels_dict:
             qrels_dict[str(qrel_row['topic'])] = {}
-        qrels_dict[str(qrel_row['topic'])][qrel_row['doc_id']] = qrel_row['relev']
+        qrels_dict[str(qrel_row['topic'])][qrel_row['doc_id']
+                                           ] = qrel_row['relev']
     return(qrels_dict)
 
+
 def split_topics(topics_df, train_split=0.6, test_split=0.5):
-    topics_train, topics_test_dev = train_test_split(topics_df, test_size=train_split)
-    topics_test, topics_dev = train_test_split(topics_test_dev, test_size=test_split)
+    topics_train, topics_test_dev = train_test_split(
+        topics_df, test_size=train_split)
+    topics_test, topics_dev = train_test_split(
+        topics_test_dev, test_size=test_split)
     return(topics_train, topics_test, topics_dev)
+
 
 def qrels_of_topics(qrels, topics):
     return(qrels[(qrels['topic'].isin(topics['topic']))])
 
+
 def split_qrels(qrels, topics_train, topics_test, topics_dev):
-    return( qrels_of_topics(qrels, topics_train),
-            qrels_of_topics(qrels, topics_test),
-            qrels_of_topics(qrels, topics_dev))
+    return(qrels_of_topics(qrels, topics_train),
+           qrels_of_topics(qrels, topics_test),
+           qrels_of_topics(qrels, topics_dev))
+
 
 def to_trec_run_file(run_df, run_params):
-    run_df[['TOPIC_NO','Q0','ID','RANK','SCORE','RUN_NAME']].to_csv('submitted_runs/'+run_params['run_id'], \
-                                                                    sep=' ', encoding='utf-8', \
-                                                                    header=False, index=False)
-def from_trec_run_file(trec_eval_file):
-    return(pandas.read_csv(trec_eval_file, delim_whitespace=True, encoding='utf-8', header=None, \
-                          names = ['TOPIC_NO','Q0','ID','RANK','SCORE','RUN_NAME']))
+    run_df[['TOPIC_NO', 'Q0', 'ID', 'RANK', 'SCORE', 'RUN_NAME']].to_csv('submitted_runs/'+run_params['run_id'],
+                                                                         sep=' ', encoding='utf-8',
+                                                                         header=False, index=False)
